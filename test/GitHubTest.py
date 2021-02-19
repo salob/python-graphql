f487b568382015c7a9a5d1bd181b75b7f3bad251
sian <sianob79@gmail.com>
2021-02-19 12:36:13 +0000

    Create GitHubTest.py

---
+++ test/GitHubTest.py
@@ -0,0 +1,19 @@
+'''
+Created on 19 Feb 2021
+
+@author: sianob
+'''
+import sys
+from src.GitHub import GitHub
+
+url = str(sys.argv[1])
+key = str(sys.argv[2])
+org = str(sys.argv[3])
+repo = str(sys.argv[4])
+issueNumber = int(sys.argv[5])
+issueTitle = str(sys.argv[6])
+
+myGithub = GitHub(url,key)
+myIssue = myGithub.getIssueByNumber(org,repo,30)
+
+assert myIssue['data']['repository']['issue'+str(issueNumber)]['title'] == issueTitle
