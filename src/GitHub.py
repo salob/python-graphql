4ce7d2e9da91505c970c312a819f70a8e2bf5b1b
sian <sianob79@gmail.com>
2021-02-19 12:18:00 +0000

    Create GitHub.py

---
+++ src/GitHub.py
@@ -0,0 +1,50 @@
+'''
+Created on 18 Feb 2021
+
+@author: sianob
+'''
+
+import requests
+
+class GitHub(object):
+    '''
+    Github object 
+    '''
+
+
+    def __init__(self,apiUrl,ghKey):
+        '''
+        Constructor
+        '''
+        self.key = ghKey
+        self.apiurl = apiUrl
+        self.header={"Authorization": "Bearer "+self.key}
+    
+    def runQuery(self,query):
+        request = requests.post(self.apiurl,headers=self.header,json={'query': query})
+        if request.status_code == 200:
+            return request.json()
+        else:
+            raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+    
+    def getIssueByNumber(self,owner,repo,issueNumber):
+        query = """
+            query{
+              repository(owner: "%s" , name: "%s") {
+                issue30: issue(number: %i) {
+                  ...IssueFragment
+                }
+              }
+            }
+            
+            fragment IssueFragment on Issue {
+              title
+              createdAt
+              body
+            }
+        
+        """ % (owner,repo,issueNumber)
+        
+        results = self.runQuery(query)
+        
+        return results
