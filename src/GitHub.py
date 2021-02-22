491f9f8b6d97107b4159dc93a67673a7b24105bc
sian <sianob79@gmail.com>
2021-02-22 09:46:32 +0000

    more methods

--- src/GitHub.py
+++ src/GitHub.py
@@ -1,14 +1,16 @@
+
 '''
 Created on 18 Feb 2021
 
-@author: sianob
+@author: salob
 '''
 
 import requests
+from datetime import datetime
 
 class GitHub(object):
     '''
-    Github object 
+    Github object
     '''
 
 
@@ -19,14 +21,15 @@ class GitHub(object):
         self.key = ghKey
         self.apiurl = apiUrl
         self.header={"Authorization": "Bearer "+self.key}
-    
+        
     def runQuery(self,query):
-        request = requests.post(self.apiurl,headers=self.header,json={'query': query})
+        s = requests.Session()
+        request = s.post(self.apiurl,headers=self.header,json={'query': query})
         if request.status_code == 200:
             return request.json()
         else:
             raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
-    
+
     def getIssueByNumber(self,owner,repo,issueNumber):
         query = """
             query{
@@ -36,15 +39,118 @@ class GitHub(object):
                 }
               }
             }
-            
+
             fragment IssueFragment on Issue {
               title
               createdAt
               body
             }
-        
+
         """ % (owner,repo,issueNumber)
-        
+
         results = self.runQuery(query)
-        
+
         return results
+    def getLabelByName(self,owner,repo,label):
+        query = """
+            query {
+              repository(owner:"%s", name:"%s") {
+                label(name:"%s") {
+                  id
+                }
+              }
+            }
+        """ % (owner,repo,label)
+        results = self.runQuery(query)
+
+        return results
+    #There should only be one actual release wiki issue per release ID
+    def getIssueByExactTitle(self,owner,repo,label,issueTitle):
+        issues = self.getIssuesByTitleKeywordAndLabel(owner,repo,label,issueTitle)
+        for issue in issues['data']['search']['nodes']:
+            if issue['title'] == issueTitle:
+                return issue
+
+    def getCommentByAuthorAndTitle(self,issue,commentAuthor,commentTitle):
+        comments = issue['comments']['edges']
+        for comment in comments:
+            if comment['node']['body'].startswith(commentTitle) and comment['node']['author']['login'] == commentAuthor:
+                return comment
+
+    #returns list of issues containing keyword
+    def getIssuesByTitleKeywordAndLabel(self,owner,repo,label,keyword):
+        query = """
+        {
+          search(query: "repo:%s/%s label:\\"%s\\" in:title %s", type: ISSUE, first: 100) {
+            nodes {
+              ... on Issue {
+                id
+                number
+                title
+                body
+                comments(first:100){
+                    edges{
+                        node{
+                          id
+                          author{
+                            login
+                          }
+                          body
+                        }
+                    }
+                }
+              }
+            }
+          }
+        }
+        """ % (owner,repo,label,keyword)
+        results = self.runQuery(query)
+
+        return results
+
+
+    def createIssue(self,input):
+        mutation = """
+          mutation{
+            createIssue(input:{%s}) {
+              issue{
+                title
+                id
+              }
+            }
+          }
+          """ % (input)
+        results = self.runQuery(mutation)
+
+        return results
+
+
+    def updateIssueComment(self,commentId,newText):
+        mutation = """
+          mutation{
+            updateIssueComment(input:{id:"%s",body:"%s"}) {
+              issueComment{
+                body
+              }
+            }
+          }
+          """ % (commentId,newText)
+        results = self.runQuery(mutation)
+
+        return results
+    def addIssueComment(self,issueId,commentTitle):
+        mutation = """
+          mutation{
+            addComment(input:{subjectId:"%s",body:"%s"}) {
+              commentEdge{
+                node{
+                  body
+                }
+              }
+            }
+          }
+          """ % (issueId,commentTitle)
+        results = self.runQuery(mutation)
+
+        return results
+
